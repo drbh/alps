b9fc83fcab39210a67f14bbfec0a87489ce1adc6
drbh <david.richard.holtz@gmail.com>
2024-02-23 08:16:43 -0500

    feat: refactor add tests and cli

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use std::collections::HashMap;
-
+use good_lp::IntoAffineExpression;
+use good_lp::{constraint, Expression, Variable as GoodVariable};
+use good_lp::{ProblemVariables, VariableDefinition};
 use serde_derive::Deserialize;
 use serde_derive::Serialize;
+use std::collections::HashMap;
+use std::ops::Div;
+use std::ops::Mul;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct UnoptimizedProblem {
@@ -266,4 +270,181 @@ impl ToString for PostfixToken {
             PostfixToken::Variable(name) => name.to_string(),
         }
     }
+}
+
+///
+
+pub fn create_variables(
+    variables: HashMap<String, Variable>,
+) -> (ProblemVariables, Vec<String>, HashMap<String, GoodVariable>) {
+    let mut variable_names = vec![];
+    let mut problem_variables = ProblemVariables::new();
+    for (name, variable) in variables {
+        let mut variable_definition = VariableDefinition::new();
+        if let Some(min) = variable.min {
+            variable_definition = variable_definition.min(min as f64);
+        } else if let Some(max) = variable.max {
+            variable_definition = variable_definition.max(max as f64);
+        }
+        problem_variables.add(variable_definition);
+        variable_names.push(name);
+    }
+
+    let variable_hashmap = {
+        let mut variable_hashmap = HashMap::new();
+        for (counter, (var, _var_def)) in problem_variables.iter_variables_with_def().enumerate() {
+            let var_name = variable_names[counter].clone();
+            variable_hashmap.insert(var_name, var);
+        }
+        variable_hashmap
+    };
+
+    (problem_variables, variable_names, variable_hashmap)
+}
+
+pub fn parse_postfix_expression(
+    postfix_tokens: Vec<&str>,
+    variable_hashmap: &HashMap<String, GoodVariable>,
+) -> Expression {
+    let mut stack: Vec<Expression> = Vec::new();
+
+    for token in postfix_tokens {
+        match token {
+            "+" | "-" | "*" | "/" => {
+                let right = stack.pop().expect("Stack underflow");
+                let left = stack.pop().expect("Stack underflow");
+
+                let result = match token {
+                    "+" => left + right,
+                    "-" => left - right,
+                    "*" => {
+                        let left_coeffs = left.clone().linear_coefficients();
+                        let left_constant = left.clone().constant();
+                        let right_constant = right.clone().constant();
+
+                        if left_coeffs.len() == 0 {
+                            right.mul(left_constant)
+                        } else {
+                            left.mul(right_constant)
+                        }
+                    }
+                    "/" => {
+                        let left_coeffs = left.clone().linear_coefficients();
+                        let left_constant = left.clone().constant();
+                        let right_constant = right.clone().constant();
+
+                        if left_coeffs.len() == 0 {
+                            right.div(left_constant)
+                        } else {
+                            left.div(right_constant)
+                        }
+                    }
+                    _ => panic!("Unsupported operator"),
+                };
+                stack.push(result);
+            }
+            _ => {
+                let operand = if let Some(var) = variable_hashmap.get(token) {
+                    Expression::from(*var)
+                } else {
+                    Expression::from(token.parse::<f64>().expect("Failed to parse float"))
+                };
+                stack.push(operand);
+            }
+        }
+    }
+
+    stack.pop().expect("No expression was created")
+}
+
+// parse postfix expressions into good_lp expressions
+// "expression": "a 2 * b + 3 +"
+// only handles ADD, SUB, MUL, DIV
+pub fn create_expression(
+    problem_expression: &str,
+    variable_hashmap: &HashMap<String, GoodVariable>,
+) -> Expression {
+    let postfix_tokens: Vec<&str> = problem_expression.split_whitespace().collect();
+    parse_postfix_expression(postfix_tokens, variable_hashmap)
+}
+
+// similar to parsing the expression but we need to map the variable names to the actual variables
+// and then create the constraints and apply the correct operator
+// handles inequality along with operators
+pub fn create_constraints(
+    problem_constraints: &Vec<Constraint>,
+    variable_hashmap: &HashMap<String, GoodVariable>,
+) -> Vec<constraint::Constraint> {
+    let mut constraints = vec![];
+    for constraint in problem_constraints {
+        let f = constraint.f.clone();
+
+        let inequalities = ["<=", ">=", "==", "<", ">"];
+
+        // check that f contains an inequality
+        let contains_inequality = inequalities.iter().any(|x| f.contains(x));
+        if !contains_inequality {
+            panic!("Constraint does not contain an inequality");
+        }
+
+        // find the inequality that is in the string
+        let my_inequality = inequalities
+            .iter()
+            .find(|x| f.contains(*x))
+            .expect("Failed to find inequality");
+
+        // split on the inequality
+        let split = f.split(my_inequality).collect::<Vec<&str>>();
+
+        let lhs = split[0].trim();
+        let rhs = split[1].trim();
+
+        let lhs_postfix = tokenize(&lhs.to_string());
+        let rhs_postfix = tokenize(&rhs.to_string());
+
+        let lhs_expression = infix_to_postfix(&lhs_postfix).unwrap();
+        let rhs_expression = infix_to_postfix(&rhs_postfix).unwrap();
+
+        let lhs_postfix_string = lhs_expression
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let rhs_postfix_string = rhs_expression
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let lhs_postfix_tokens: Vec<&str> = lhs_postfix_string.split_whitespace().collect();
+        let rhs_postfix_tokens: Vec<&str> = rhs_postfix_string.split_whitespace().collect();
+
+        let lhs_expression = parse_postfix_expression(lhs_postfix_tokens, variable_hashmap);
+        let rhs_expression = parse_postfix_expression(rhs_postfix_tokens, variable_hashmap);
+
+        // use the my_inequality to create the constraint
+        let constraint = match *my_inequality {
+            "<=" => good_lp::constraint!(lhs_expression <= rhs_expression),
+            ">=" => good_lp::constraint!(lhs_expression >= rhs_expression),
+            "==" => good_lp::constraint!(lhs_expression == rhs_expression),
+            // throw an error if the inequality is not supported
+            _ => panic!("Unsupported inequality"),
+        };
+
+        constraints.push(constraint);
+    }
+    constraints
+}
+
+pub fn parse_objective_expression(objective: &String) -> String {
+    // let input_str = "( ( 3 * a ) + b ) * 3".to_string(); // 3 a * b + 3 *
+    let original_tokens = tokenize(objective);
+    let result = infix_to_postfix(&original_tokens).unwrap();
+    let postfix_string = result
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+    postfix_string
 }
---
+++ src/tests.rs
@@ -0,0 +1,184 @@
+// test create_expression
+#[cfg(test)]
+mod tests {
+    use crate::{
+        //
+        create_constraints,
+        create_expression,
+        create_variables,
+        parse_objective_expression,
+        UnoptimizedProblem,
+    };
+
+    #[test]
+    fn test_create_expression_simple() {
+        let json_problem = r#"
+        {
+            "variables": {
+              "a": {"max": 1},
+              "b": {"min": 2, "max": 10}
+            },
+            "objective": {
+              "goal": "max",
+              "expression": "10 + a - b"
+            },
+            "constraints": [
+              {
+                "name": "flour",
+                "f": "3 + a >= b"
+              },
+              {
+                "name": "milk",
+                "f": "3 + a >= b"
+              }
+            ]
+          }
+        "#;
+        let problem: UnoptimizedProblem = serde_json::from_str(json_problem).unwrap();
+        let (_problem_variables, _variable_names, variable_hashmap) =
+            create_variables(problem.variables);
+
+        let parsed_expression = parse_objective_expression(&problem.objective.expression);
+        let expression = create_expression(&parsed_expression, &variable_hashmap);
+        let string_expression = format!("{:?}", expression);
+        let acceptable_expressions = vec![
+            //
+            "v1 + -1 v0 + 10".to_string(),
+            "-1 v1 + v0 + 10".to_string(),
+        ];
+
+        let matched = acceptable_expressions
+            .iter()
+            .any(|x| x == &string_expression);
+
+        assert!(matched);
+    }
+
+    #[test]
+    fn test_create_constraint_simple() {
+        let json_problem = r#"
+        {
+            "variables": {
+              "a": {"max": 1},
+              "b": {"min": 2, "max": 10}
+            },
+            "objective": {
+              "goal": "max",
+              "expression": "a + ( -1 * b ) + 5"
+            },
+            "constraints": [
+              {
+                "name": "flour",
+                "f": "3 + a >= b"
+              },
+              {
+                "name": "milk",
+                "f": "6 + a >= b"
+              }
+            ]
+          }
+        "#;
+        let problem: UnoptimizedProblem = serde_json::from_str(json_problem).unwrap();
+        let (_problem_variables, _variable_names, variable_hashmap) =
+            create_variables(problem.variables);
+
+        let parsed_expression = parse_objective_expression(&problem.objective.expression);
+        let expression = create_expression(&parsed_expression, &variable_hashmap);
+        let string_expression = format!("{:?}", expression);
+
+        let constraints = create_constraints(&problem.constraints, &variable_hashmap);
+        assert_eq!(constraints.len(), 2);
+
+        let mut expected_constraints =
+            vec!["-1 v1 + v0 <= 3".to_string(), "-1 v1 + v0 <= 6".to_string()];
+        let mut other_expected_constraints =
+            vec!["v1 + -1 v0 <= 3".to_string(), "v1 + -1 v0 <= 6".to_string()];
+
+        // sort the vectors so that we can compare them
+        expected_constraints.sort();
+        other_expected_constraints.sort(); // TODO: integrate this into the test
+
+        let acceptable_expressions_list = vec![
+            expected_constraints.clone(),
+            other_expected_constraints.clone(),
+        ];
+
+        let mut actual_constraints = constraints
+            .iter()
+            .map(|x| format!("{:?}", x))
+            .collect::<Vec<String>>();
+
+        actual_constraints.sort();
+
+        let mut matches = vec![];
+        for acceptable_expressions in acceptable_expressions_list {
+            let matched = acceptable_expressions
+                .iter()
+                .any(|x| actual_constraints.contains(x));
+            matches.push(matched);
+        }
+
+        assert!(matches.iter().any(|x| x == &true));
+
+        // must match one of the acceptable expressions
+        let acceptable_expressions = vec![
+            "-1 v1 + v0 + 5".to_string(),
+            "v1 + -1 v0 + 5".to_string(),
+            "-1 v1 + v0 + 5".to_string(),
+        ];
+
+        let matched = acceptable_expressions
+            .iter()
+            .any(|x| x == &string_expression);
+
+        assert!(matched);
+    }
+
+    #[test]
+    fn test_create_expression() {
+        let json_problem = r#"
+        {
+            "variables": {
+              "a": {"max": 1},
+              "b": {"min": 2, "max": 10}
+            },
+            "objective": {
+              "goal": "max",
+              "expression": "a + ( -1 * b ) + 5"
+            },
+            "constraints": [
+              {
+                "name": "flour",
+                "f": "3 + a >= b"
+              },
+              {
+                "name": "milk",
+                "f": "6 + a >= b"
+              }
+            ]
+          }
+        "#;
+        let problem: UnoptimizedProblem = serde_json::from_str(json_problem).unwrap();
+        let (_problem_variables, _variable_names, variable_hashmap) =
+            create_variables(problem.variables);
+
+        let parsed_expression = parse_objective_expression(&problem.objective.expression);
+        let expression = create_expression(&parsed_expression, &variable_hashmap);
+        let string_expression = format!("{:?}", expression);
+
+        // a+2-b+3
+
+        // must match one of the acceptable expressions
+        let acceptable_expressions = vec![
+            "v1 + -1 v0 + 5".to_string(),
+            "v0 + -1 v1 + 5".to_string(),
+            "-1 v1 + v0 + 5".to_string(),
+        ];
+
+        let matched = acceptable_expressions
+            .iter()
+            .any(|x| x == &string_expression);
+
+        assert!(matched);
+    }
+}
